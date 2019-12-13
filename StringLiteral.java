52085a7b1833b80730ccbdbd846ec163a26875e9
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2019-12-13 12:54:33 +0530

    Add files via upload
    String literal

---
+++ StringLiteral.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class StringLiteral {
+	public static void main(String args[])
+	{
+		String str="I, am, going, to, this, string by, space.";
+		String[] str1=str.split(",");
+		for(String a:str1)
+		System.out.print(a);
+	}
+
+}
