ac802f0dc38307b75aab1ca363f17d029e51511c
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2019-12-13 12:53:54 +0530

    Add files via upload
    String using intern

---
+++ StringIntern.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class StringIntern {
+	public static void main(String args[])
+	{
+		String a="well";
+		String ab="All is " + a;
+		String abc="All is well";
+		String abcd="All is well";
+		System.out.println(abc.hashCode());
+		System.out.println(ab.hashCode());
+		System.out.println(abc == ab);
+		System.out.println(abc.hashCode());
+		System.out.println(abcd.hashCode());
+		
+	}
+
+}
