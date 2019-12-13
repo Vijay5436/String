93332a0f733fd38a0efff20391984ca4593d605a
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2019-12-13 12:52:59 +0530

    String Comparing

---
+++ StringCompare.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class StringCompare {
+public static void main(String args[])
+{
+	String s1,s2;
+	s1=new String("Rajinikanth");
+	s2=new String("Rajinikanth");
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(s1==s2);
+	System.out.println(s1.hashCode());
+	System.out.println(s2.hashCode());
+	s2=s1+s2;
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(s1.hashCode());
+	System.out.println(s2.hashCode());
+}
+}
