f5de71bc5634a859c35710e96912c1b4ec7318ba
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2020-01-01 15:02:02 +0530

    Add files via upload

---
+++ charAtExample2.java
@@ -0,0 +1,10 @@
+package Srtingmethods;
+
+public class charAtExample2 {
+	public static void main(String[] args) {
+	String name="Face@2.0";
+	char ch=name.charAt(10);
+	System.out.println(ch);//it is error
+}
+
+}
