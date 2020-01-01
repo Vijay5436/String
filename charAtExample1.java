bdb0a693c4b2f3d0215e36490b941a3ce8ae7af7
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2020-01-01 15:01:33 +0530

    Add files via upload

---
+++ charAtExample1.java
@@ -0,0 +1,12 @@
+package Srtingmethods;
+
+public class charAtExample1 {
+	public static void main(String[] args)
+	{
+		String name="Happy Christmass";
+		char ch=name.charAt(7);
+		//return the char value at 7th index.
+		System.out.print(ch);//it starts from 0 & goes upto n-1.
+	}
+
+}
