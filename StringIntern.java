262b443de95b4493fa1fac9eab7f158df60009d7
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2020-01-01 14:57:44 +0530

    Add files via upload

--- StringIntern.java
+++ StringIntern.java
@@ -9,7 +9,7 @@ public class StringIntern {
 		String abcd="All is well";
 		System.out.println(abc.hashCode());
 		System.out.println(ab.hashCode());
-		System.out.println(abc == ab);
+		System.out.println(abc == ab.intern());
 		System.out.println(abc.hashCode());
 		System.out.println(abcd.hashCode());
 		
